18f9dfcbc3b20300b72be19a2e547ed116b10c99
wavim <wavim.wu@gmail.com>
2025-09-06 21:04:44 +0800

    feat: reworked demo.colors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { sort2D, Vec2 } from "./ts/hilbert-sort.js";
+import { sort2D, sort3D, Vec2, Vec3 } from "./ts/hilbert-sort.js";
 
 const pointsCntInput = document.getElementById("points-cnt") as HTMLInputElement;
 const pointsStdInput = document.getElementById("points-std") as HTMLInputElement;
@@ -50,3 +50,45 @@ function updatePoints(): void {
 updatePoints();
 
 pointsCntInput.oninput = pointsStdInput.oninput = updatePoints;
+
+const colorsCntInput = document.getElementById("colors-cnt") as HTMLInputElement;
+const colorsBefore = document.getElementById("colors-before") as HTMLCanvasElement;
+const colorsResult = document.getElementById("colors-result") as HTMLCanvasElement;
+
+function randomColors(n: number): Vec3[] {
+	const colors: Vec3[] = [];
+
+	for (let i = 0; i < n; i++) {
+		colors.push([
+			Math.floor(Math.random() * 256),
+			Math.floor(Math.random() * 256),
+			Math.floor(Math.random() * 256),
+		]);
+	}
+	return colors;
+}
+
+function renderColors(canvas: HTMLCanvasElement, colors: Vec3[]): void {
+	const ctx = canvas.getContext("2d")!;
+
+	const w = canvas.width / colors.length;
+	const h = canvas.height;
+
+	for (let i = 0; i < colors.length; i++) {
+		ctx.fillStyle = `rgb(${colors[i].join(" ")})`;
+		ctx.fillRect(w * i, 0, w + 1, h);
+	}
+}
+
+function updateColors(): void {
+	// reset canvas
+	colorsBefore.width = colorsResult.width = colorsBefore.clientWidth;
+	colorsBefore.height = colorsResult.height = colorsBefore.clientHeight;
+
+	const colors = randomColors(colorsCntInput.valueAsNumber);
+	renderColors(colorsBefore, colors);
+	renderColors(colorsResult, sort3D(colors));
+}
+updateColors();
+
+colorsCntInput.oninput = updateColors;
